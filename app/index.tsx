20340d1bd0d94dbe27781844a41513b356654383
Meesam <meesam.engineer@gmail.com>
2025-05-12 17:27:02 +0530

    Initial setup

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello World, this is real state app .</Text>
+    </View>
+  );
+}
